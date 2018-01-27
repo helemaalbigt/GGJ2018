05f942d6c84e7e37f34695b79202347f3dbc7a03
ThomasVB <van.bouwel.thomas@gmail.com>
2018-01-27 13:29:18 -0500

    play animation step

---
+++ Assets/#Project/_scripts/GameSteps/PlayAnimtationStep.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayAnimtationStep : GameStep
+{
+    public Animator _animator;
+    public string _animation;
+
+    public override void StartStep() {
+        StartCoroutine(PlayAnimation());
+    }
+
+    IEnumerator PlayAnimation() {
+        _animator.Play(_animation);
+
+        while (_animator.GetCurrentAnimatorStateInfo(0).IsName(_animation)) 
+            yield return null;
+
+        Next();
+    }
+}
