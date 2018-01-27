0fe08ce994d6fb6ed388026ce5ca7ba26bf79f05
ThomasVB <van.bouwel.thomas@gmail.com>
2018-01-27 12:41:29 -0500

    actually this is the step logic lol

--- Assets/#Project/_scripts/GameSteps/GameStep.cs
+++ Assets/#Project/_scripts/GameSteps/GameStep.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public abstract class GameStep : MonoBehaviour
 {
+    public bool isFirstStep;
+    public GameStep nextStep;
+
+    void Start() {
+        if (isFirstStep) 
+            StartStep();
+    }
+
     public abstract void StartStep();
 
-    public void GoToStep(GameStep step) {
-        step.StartStep();
+    public void Next() {
+        if(nextStep != null)
+            nextStep.StartStep();
     }
 }
--- Assets/#Project/_scripts/GameSteps/StartscreenStep.cs
+++ Assets/#Project/_scripts/GameSteps/StartscreenStep.cs
@@ -1,9 +1,43 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
 
-public class StartscreenStep : GameStep {
+public class StartscreenStep : GameStep
+{
+    public GameObject _titleScreen;
+    public Text _startPrompt;
+
     public override void StartStep() {
-        throw new System.NotImplementedException();
+        SetupScene();
+        StartCoroutine(WaitForUsers());
+    }
+
+    private void SetupScene() {
+        _titleScreen.SetActive(true);
+        _startPrompt.text = "press space to start";
+    }
+
+    IEnumerator WaitForUsers() {
+        bool pcReady = false;
+        bool vrReady = false;
+
+        while (!pcReady || !vrReady) {
+            if (Input.GetKeyDown(KeyCode.Space) && !pcReady) {
+                pcReady = true;
+                Debug.Log("yup " + vrReady);
+                if(!vrReady)
+                    _startPrompt.text = "waiting for VR player...";
+            }
+
+            if (XRDevice.userPresence == UserPresenceState.Present) {
+                vrReady = true;
+            }
+
+            yield return null;
+        }
+
+        Debug.Log("GAME START");
+        Next();
     }
 }
