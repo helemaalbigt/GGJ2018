b93fa00b2ee5cf0510a13a26ed4cdde179ca287e
ThomasVB <van.bouwel.thomas@gmail.com>
2018-01-27 11:38:57 -0500

    gamesteps script

---
+++ Assets/#Project/_scripts/GameSteps/GameStep.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameStep : MonoBehaviour
+{
+    public abstract void StartStep();
+
+    public void GoToStep(GameStep step) {
+        step.StartStep();
+    }
+}
---
+++ Assets/#Project/_scripts/GameSteps/StartscreenStep.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartscreenStep : GameStep {
+    public override void StartStep() {
+        throw new System.NotImplementedException();
+    }
+}
