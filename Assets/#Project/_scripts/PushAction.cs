47521067bccde6a96dba556edef86f9384c20f03
SU <contact@ulfsson.com>
2018-01-27 19:40:14 -0500

    Improved push action script. Material improvements in the corridor.

---
+++ Assets/#Project/_scripts/PushAction.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushAction : MonoBehaviour {
+
+	public Vector3 PushVector;
+	public Vector3 PushRotation;
+
+	public float TimeToPush = 1.0f;
+	public float DelayBeforePush = 0.5f;
+	public bool EnableGravityAfterPush = true;
+
+	private Rigidbody _rigidBody;
+
+	void Start () {
+		_rigidBody = this.GetComponent<Rigidbody> ();
+		Push ();
+	}
+
+	public void Push() {
+		StartCoroutine (_Push());
+	}
+
+	IEnumerator _Push() {
+		yield return new WaitForSeconds(DelayBeforePush);
+
+		// TODO: Play a sound here
+
+		Vector3 StartPosition = transform.position;
+		Vector3 EndPosition = StartPosition + PushVector;
+		Quaternion StartRotation = transform.rotation;
+		Quaternion EndRotation = StartRotation * Quaternion.Euler(PushRotation);
+
+		float StartTime = Time.time;
+		float TimeElapsed = 0;
+
+		while (TimeElapsed < TimeToPush) {
+			TimeElapsed = Time.time - StartTime;
+			transform.position = Vector3.Lerp (StartPosition, EndPosition, TimeElapsed / TimeToPush);
+			transform.rotation = Quaternion.Lerp(StartRotation, EndRotation, TimeElapsed / TimeToPush);
+			yield return null;
+		}
+
+		if (_rigidBody && EnableGravityAfterPush) {
+			_rigidBody.useGravity = true;
+			_rigidBody.isKinematic = false;
+		}
+	}
+}
