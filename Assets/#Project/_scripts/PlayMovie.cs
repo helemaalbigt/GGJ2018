764b965392296ce1801492f2546712937b33e141
Richard Kopelow <richardkopelow@gmail.com>
2018-01-28 13:08:34 -0500

    Added a new slam door sound Movies are working Added Keyhole Added background sound

---
+++ Assets/#Project/_scripts/PlayMovie.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMovie : MonoBehaviour
+{
+    MovieTexture movie;
+    void Start()
+    {
+        movie = ((MovieTexture)GetComponent<Renderer>().material.mainTexture);
+        movie.Play();
+        movie.loop = true;
+    }
+}
