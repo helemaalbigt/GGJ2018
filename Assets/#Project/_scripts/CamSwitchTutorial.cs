478c1634e69d4b86d1c3f03136ee1af1dfe8b176
ThomasVB <van.bouwel.thomas@gmail.com>
2018-01-28 00:45:37 -0500

    nice camtext

---
+++ Assets/#Project/_scripts/CamSwitchTutorial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitchTutorial : MonoBehaviour
+{
+    public GameObject tutorialText;
+
+    private bool pressed;
+
+	void Start () {
+	    StartCoroutine(Blink());
+	}
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Alpha2))
+            pressed = true;
+    }
+
+    IEnumerator Blink() {
+        while(!pressed) {
+            yield return new WaitForSeconds(.75f);
+            tutorialText.SetActive(false);
+            yield return new WaitForSeconds(.75f);
+            tutorialText.SetActive(true);
+        }
+
+        tutorialText.SetActive(true);
+    }
+}
---
+++ Assets/#Project/_scripts/CamTime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CamTime : MonoBehaviour
+{
+    public Text _text;
+
+	void Update () {
+	    _text.text = "11-10-84 " + IntToTwoDigitString(DateTime.Now.Hour) + ":" + IntToTwoDigitString(DateTime.Now.Minute) +
+	                ":" + IntToTwoDigitString(DateTime.Now.Second);
+	}
+
+    private string IntToTwoDigitString(int value) {
+        return (value < 10) ? "0" + value : value.ToString();
+    }
+}
