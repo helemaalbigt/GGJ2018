0574fc2f5b34abdda3d5d57af034249b4ffe81ff
SU <contact@ulfsson.com>
2018-01-27 20:30:21 -0500

    New killer collider step. Material changes.

---
+++ Assets/#Project/_scripts/KillerCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KillerCollider : MonoBehaviour {
+
+	public Collider collider;
+	public List<string> tags;
+	public UnityEvent collisionEvents;
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<Collider> ();
+	}
+	
+	void OnCollisionEnter(Collision col) {
+		Debug.Log (this.name + " collided with " + col.gameObject + " (" + col.gameObject.tag + ")");
+		if (tags.Contains (col.gameObject.tag) || tags.Count == 0) {	// Tags list empty, or the colliding object is in the list
+			Debug.Log ("Invoking collision event(s)!");
+			if (collisionEvents != null) {
+				collisionEvents.Invoke ();
+			}
+		}
+	}
+}
