00ab7b04ddce00d21ede911f1f039a0b8f37a121
Richard Kopelow <richardkopelow@gmail.com>
2018-01-27 23:25:54 -0500

    Key clangs Panel should clang, I can't test it

---
+++ Assets/#Project/_scripts/EventOnCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventOnCollision : MonoBehaviour
+{
+    public bool OneTime = true;
+    public UnityEvent OnCollision;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        OnCollision.Invoke();
+        if (OneTime)
+        {
+            enabled = false;
+        }
+    }
+}
